5273c5ab6f8c19201a32cf1c66cbb24d4cf9b4bd
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2023-09-29 18:52:10 +0100

    Creating customer model

--- src/main/java/com/tutorial/tunjiTech/TunjiTechApplication.java
+++ src/main/java/com/tutorial/tunjiTech/TunjiTechApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -15,22 +16,76 @@ public class TunjiTechApplication {
 
 		SpringApplication.run(TunjiTechApplication.class, args);
 	}
+	class customer{
+		private Integer id;
+		private String name;
+		private String email;
+		private Integer age;
 
-	@GetMapping("/greet")
-	public GreetResponse greet()
-	{
-		GreetResponse response = new GreetResponse("Hello",
-				List.of("Java", "Golan", "JavaScript"),
-				new Person("Alex")
-		);
-		return response;
-	}
+		public customer() {
+		}
 
-	record Person(String name){}
-	record GreetResponse(
-			String greet,
-			List<String> favProgLang,
-			Person person){
+		public customer(Integer id, String name, String email, Integer age) {
+			this.id = id;
+			this.name = name;
+			this.email = email;
+			this.age = age;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Integer getAge() {
+			return age;
+		}
+
+		public void setAge(Integer age) {
+			this.age = age;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			customer customer = (customer) o;
+			return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id, name, email, age);
+		}
+
+		@Override
+		public String toString() {
+			return "customer{" +
+					"id=" + id +
+					", name='" + name + '\'' +
+					", email='" + email + '\'' +
+					", age=" + age +
+					'}';
+		}
 
 	}
 }
